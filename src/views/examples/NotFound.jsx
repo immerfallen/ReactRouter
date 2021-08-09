bde4cf98b6969d40e861379543ae72389456ea9e
Maro de Melo <marodemelo10@gmail.com>
2021-08-09 20:49:22 -0300

    implementando um compoenente para pagina nao encontrada

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = (props) => (
+  <div className="NotFound">
+    <h1 >404</h1>
+    <p>Ooopss.. Página nao encontrada</p>
+  </div>
+);
+
+export default NotFound;
